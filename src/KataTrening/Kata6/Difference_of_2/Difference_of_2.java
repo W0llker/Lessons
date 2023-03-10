1ce13bb184c24b8c8a03ded2b557b4eda1320add
W0llker <reklowwolker@gmail.com>
2023-03-10 23:35:36 +0300

    update

--- src/KataTrening/Kata6/Difference_of_2/Difference_of_2.java
+++ src/KataTrening/Kata6/Difference_of_2/Difference_of_2.java
@@ -3,16 +3,13 @@ package KataTrening.Kata6.Difference_of_2;
 import java.util.*;
 
 public class Difference_of_2 {
-    public static void main(String[] args) {
-        Difference_of_2.twosDifference(new int[] {1, 3, 4, 6});
-    }
     public static int[][] twosDifference(int[] array) {
         Arrays.sort(array);
         ArrayList<int[]> arrayList = new ArrayList<>();
         for (int j : array) {
             for (int integer : array) {
                 if ((j + 2) == integer) {
-                    arrayList.add(new int[]{j , integer});
+                    arrayList.add(new int[]{j, integer});
                 }
             }
         }
--- src/byItAcademy/Lesson_3/HomeWork.java
+++ src/byItAcademy/Lesson_3/HomeWork.java
@@ -1,7 +1,24 @@
 package byItAcademy.Lesson_3;
 
+import java.util.Scanner;
+
+/*
+1. На вход даны 3 числа. Вывести в консоль только четные числа
+2. На вход даны 3 числа. Вывести в консоль только те числа, которые делятся на 2 и на 5
+*/
 public class HomeWork {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите А");
+        int a = scanner.nextInt();
+        System.out.println("Введите B");
+        int b = scanner.nextInt();
+        System.out.println("Введите C");
+        int c = scanner.nextInt();
+        zadanie_1(a, b, c);
+    }
 
+    public static void zadanie_1(int a, int b, int c) {
+        
     }
 }
