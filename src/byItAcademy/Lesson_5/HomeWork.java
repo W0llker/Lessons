04f714ac8d2a9000fe1b6ea9f3286bba2ccde6fa
W0llker <118970644+W0llker@users.noreply.github.com>
2023-03-17 20:12:13 +0300

    test

--- src/byItAcademy/Lesson_5/HomeWork.java
+++ src/byItAcademy/Lesson_5/HomeWork.java
@@ -2,18 +2,6 @@ package byItAcademy.Lesson_5;
 
 public class HomeWork {
     public static void main(String[] args) {
-        int[] mass = new int[5];
-        int[] ms = new int[mass.length+2];
-        int c = 0;
-        int s = 1;
-        while (s < 10000) {
-            if(c >= s){
-                s += c;
-                System.out.println(s);
-            }else {
-                c += s;
-                System.out.println(c);
-            }
-        }
+
     }
 }
--- src/byItAcademy/Lesson_5/Main.java
+++ src/byItAcademy/Lesson_5/Main.java
@@ -2,6 +2,18 @@ package byItAcademy.Lesson_5;
 
 public class Main {
     public static void main(String[] args) {
-
+        int[] mass = new int[5];
+        int[] ms = new int[mass.length+2];
+        int c = 0;
+        int s = 1;
+        while (s < 10000) {
+            if(c >= s){
+                s += c;
+                System.out.println(s);
+            }else {
+                c += s;
+                System.out.println(c);
+            }
+        }
     }
 }
