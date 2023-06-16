3909cf976bf1f45e859648b3337824440e1be1a5
W0llker <118970644+W0llker@users.noreply.github.com>
2023-06-16 17:40:20 +0300

    UpdateProject

--- src/byItAcademy/Lesson_20/HomeWork/DataFormatedMain.java
+++ src/byItAcademy/Lesson_20/HomeWork/DataFormatedMain.java
@@ -5,14 +5,13 @@ package byItAcademy.Lesson_20.HomeWork;
  */
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 
 public class DataFormatedMain {
     public static void main(String[] args) {
-        System.out.println(DataFormatedMain.dateformated(LocalDateTime.now(),DateTimeFormatter.ofPattern("GGGG qqqq EEEE MMM yyyy',' 'время:' HH:mm:ss")));
-        System.out.println(DataFormatedMain.dateformated(LocalDateTime.now(),DateTimeFormatter.ofPattern("GGGG qqqq EEEE MMM yyyy',' 'время:' hh:mm:ss a")));
+        System.out.println(DataFormatedMain.dateFormated(LocalDateTime.now(),DateTimeFormatter.ofPattern("GGGG qqqq EEEE MMM yyyy',' 'время:' HH:mm:ss")));
+        System.out.println(DataFormatedMain.dateFormated(LocalDateTime.now(),DateTimeFormatter.ofPattern("GGGG qqqq EEEE MMM yyyy',' 'время:' hh:mm:ss a")));
     }
-    static String dateformated(LocalDateTime localDateTime, DateTimeFormatter dateTimeFormatter) {
+    static String dateFormated(LocalDateTime localDateTime, DateTimeFormatter dateTimeFormatter) {
         return localDateTime.format(dateTimeFormatter);
     }
 }
